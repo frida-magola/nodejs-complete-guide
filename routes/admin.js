dccfabc286a4a89fca5c13f840d7fcf20e8b1c89
demwavigne <mwalilanyira@gmail.com>
2019-09-20 20:14:48 +0200

    adding css styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
+
 
 // admin/add-produit => GET
 router.get('/add-product', (req,res,next) => {
     
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    //first way for using path by importing path to above
+    // res.sendFile(path.join(__dirname,'../','views','add-product.html')); 
+    
+    //second way for using path it by importing an external confuguration the root directory 
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 // admin/add-product => POST
