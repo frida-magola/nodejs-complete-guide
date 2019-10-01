9f93ce893e5cf443e75f5a6567ba4662ad9609a9
demwavigne <mwalilanyira@gmail.com>
2019-10-01 13:40:53 +0200

    adding the models pattern MVC

--- controllers/products.js
+++ controllers/products.js
@@ -1,37 +1,32 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProducts = (req,res,next) => {
-    
-    //first way for using path by importing path to above
-    // res.sendFile(path.join(__dirname,'../','views','add-product.html')); 
-    
-    //second way for using path it by importing an external confuguration the root directory 
-    // res.sendFile(path.join(rootDir,'views','add-product.html'));
-    
-    //render view with pug template engine
+    //render view with ejs template engine
     res.render('add-product',{
         pageTitle:'add-product',
         path:'/admin/add-product',
-        activeAddProduct: true, //add for activing the menus in handlebars template engine in express
-        productCSS: true, // active the css style here handlebars require 
-        formCSS: true, // active the css style here handlebars require 
     });
 }
 
 exports.postAddProducts = (req,res,next) => {
-    // console.log(req.body);
-    products.push({title:req.body.title});
+    
+    const product = new Product(req.body.title);
+    //save the product
+    product.save();
+    
     res.redirect('/');
 }
 
-exports.getProducts = (req,res,next) => {
-    //render the pug template
-    res.render('shop',{
-        prods:products,
-        pageTitle:'Shop',
-        path:'/',
-        hasProducts:products.length > 0, //add for handlebars template engine
-        activeShop: true, //add for activing the menus in handlebars template engine in express
-        productCSS: true, // active the css style here handlebars require 
+exports.getProducts = (req,res,next) => {  
+    //fetch all the products
+    Product.fetchAll(products => {
+       //render the ejs template
+        res.render('shop',{
+            prods:products,
+            pageTitle:'Shop',
+            path:'/',
+
+        }); 
     });
+    
 }
